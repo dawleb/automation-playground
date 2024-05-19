79b7b9ded14454cc772d88519e1305d1ef98c25a
ice9 <bgice5017@gmail.com>
2024-05-19 19:40:39 +0200

    fix welcome

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import pyramidBackground from './pyramid_background.jpg';
+
+const welcomeStyle = {
+  color: 'white',
+  textAlign: 'center',
+  lineHeight: '100vh',
+};
 
 function Welcome() {
   return (
-    <div>
-      <h1>Welcome!</h1>
+    <div style={{ backgroundImage: `url(${pyramidBackground})`, height: '100vh', backgroundSize: 'cover', backgroundPosition: 'center' }}>
+      <h1 style={welcomeStyle}>Welcome!</h1>
     </div>
   );
 }
