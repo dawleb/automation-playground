5fa99d7b2eccb569b28e2964dcde93619d3f0fe2
ice9 <bgice5017@gmail.com>
2025-01-16 15:04:03 +0100

    fix start script

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host: "",
     user: "",
     password: "",
