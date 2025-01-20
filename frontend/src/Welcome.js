e946d3ed565f7e0004cf7d12ee509ec376743a85
ice9 <bgice5017@gmail.com>
2025-01-20 17:55:41 +0100

    fix ui

--- frontend/src/Welcome.js
+++ frontend/src/Welcome.js
@@ -1,16 +1,9 @@
 import React from 'react';
-import pyramidBackground from './assets/pyramid_background.jpg';
-
-const welcomeStyle = {
-  color: 'white',
-  textAlign: 'center',
-  lineHeight: '100vh',
-};
 
 function Welcome() {
   return (
-    <div style={{ backgroundImage: `url(${pyramidBackground})`, height: '100vh', backgroundSize: 'cover', backgroundPosition: 'center' }}>
-      <h1 style={welcomeStyle}>Welcome!</h1>
+    <div className="background welcome">
+      <h1>Welcome!</h1>
     </div>
   );
 }
