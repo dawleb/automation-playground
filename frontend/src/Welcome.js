b575b0b8c86472a7a9aaad70191fd8defae7752e
ice9 <bgice5017@gmail.com>
2025-01-26 16:43:57 +0100

    fix config

--- frontend/src/Welcome.js
+++ frontend/src/Welcome.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import API_URL from './apiConfig';
+import API_URL from './config';
 
 
 function Welcome() {
