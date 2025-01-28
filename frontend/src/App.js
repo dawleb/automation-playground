263e951c069a610e1730c1eda6929984e4b26208
ice9 <bgice5017@gmail.com>
2025-01-28 07:57:09 +0100

    fix routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,32 @@
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import Login from './Login';
 import Welcome from './Welcome';
 import { Navigate } from 'react-router-dom';
 
+const router = createHashRouter(
+  [
+    { path: '/', element: <Navigate to="/login" replace /> },
+    { path: '/login', element: <Login /> },
+    { path: '/welcome', element: <Welcome /> },
+    { path: '*', element: <Login /> },
+  ],
+  {
+    future: {
+      v7_relativeSplatPath: true,
+      v7_fetcherPersist: true,
+      v7_normalizeFormMethod: true,
+      v7_partialHydration: true,
+      v7_skipActionErrorRevalidation: true,
+    },
+  }
+);
+
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/login" element={<Login />} exact />
-        <Route path="/welcome" element={<Welcome />} exact />
-        <Route path="*" element={<Login />} />
-      </Routes>
-    </Router>
+    <RouterProvider
+      future={{ v7_startTransition: true }}
+      router={router}
+    />
   );
 }
 
