7af1c86289280e1270b632cbd8f1c94bb86079d4
Dawid Lebioda <dawid.lebioda@gmail.com>
2025-06-06 12:10:54 +0200

    remve comments

--- tests/playwright-pom/e2e/auth.setup.ts
+++ tests/playwright-pom/e2e/auth.setup.ts
@@ -2,11 +2,7 @@ import { test as setup, expect } from '../fixtures/base-test.ts';
 
 import path from 'path';
 
-// const authFile = path.join(__dirname, '../.auth/user.json');
-// const authFile = path.join(__dirname, '..', '.auth', 'user.json');
-
 import { fileURLToPath } from 'url';
-// import path from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
